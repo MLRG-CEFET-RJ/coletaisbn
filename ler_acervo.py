be5756a88d4ea49dc2471d8a12079f1ec0861833
DaniFavoreto <39687418+DaniFavoreto@users.noreply.github.com>
2019-08-03 15:08:51 -0300

    03/08

--- ler_acervo.py
+++ ler_acervo.py
@@ -11,6 +11,7 @@ import re
 import argparse
 import os
 import json
+from json2xml import json2xml
 
 def print_coluna(validator,file_name,titulo):
     if(validator =='.csv'):
@@ -33,6 +34,15 @@ class Acervo():
         self.file = file
         
     def only_isbn(self):
+        
+        path_df   = './New_csvs'
+        path_list ='./ISBN_lists'
+        
+        if not os.path.isdir(path_df):
+            os.mkdir(path_df)
+        if not os.path.isdir(path_list):
+            os.mkdir(path_list)
+            
         if(self.isbn_header==None):
             print("Erro:indique o titulo que contem os numeros isbn")
         else:
@@ -50,16 +60,19 @@ class Acervo():
             
             self.df = self.df.drop(self.df.index[positions_list])
             self.df[self.isbn_header]=list_
-            self.df.to_csv('{}_novo.csv'.format(self.file),index=None)
+            self.df.to_csv('{}/{}_novo.csv'.format(path_df,self.file),index=None)
             list_ = pd.DataFrame(list_)
-            list_.to_csv('{}_isbn.csv'.format(self.file),index=None)
+            list_.to_csv('{}/{}_isbn.csv'.format(path_list,self.file),index=None)
+        
+        print("lista com numeros isbn gerada.")
             
 class Constructor():   
-    def __init__(self,df,isbn_list,isbn_title,new_title):
+    def __init__(self,df,isbn_list,isbn_title,new_title,xml):
         self.df          = df
         self.isbn_list   = isbn_list
         self.isbn_title  = isbn_title
         self.new_title   = new_title 
+        self.xml         = xml
         
     def generate_json(self):
         
@@ -72,76 +85,102 @@ class Constructor():
             my_dict = {}
             
         isbn_list = list(self.isbn_list['0'])
-        titulo_list = list(self.df[self.isbn_title])
-        concatenate_list = []
         
-        for isbn in isbn_list:
-            if (not ('{}'.format(isbn) in my_dict)):
-                my_dict[str(isbn)] = {}
+        a = self.new_title.split(',')
+        b = self.isbn_title.split(',')
+        
+        for j in range (len(a)):
             
-        for i, titulos in enumerate(titulo_list):
-            concatenate_list.append(((isbn_list[i]),(self.new_title,titulos)))
+            titulo_list = list(self.df[b[j]])
+            concatenate_list = []
             
-        for data in concatenate_list:
-            my_dict[str(data[0])][data[1][0]]=data[1][1]
-            
+            for isbn in isbn_list:
+                if (not ('{}'.format(isbn) in my_dict)):
+                    my_dict[str(isbn)] = {}
+                
+            for i, titulos in enumerate(titulo_list):
+                concatenate_list.append(((isbn_list[i]),(a[j],titulos)))
+                
+            for data in concatenate_list:
+                my_dict[str(data[0])][data[1][0]]=data[1][1]
+                
         if not os.path.isdir(path):
             os.mkdir(path)
             
         with open('{}/dadosuniversidades.json'.format(path), 'w') as outfile:
                 json.dump(my_dict, outfile)
+                print("arquivo json gerado com sucesso.")
         
-        return my_dict
+        xml=json2xml.Json2xml(my_dict).to_xml()
+        
+        if(self.xml):
+            path = './xmlfiles'
+            
+            if not os.path.isdir(path):
+               os.mkdir(path)
+            
+            myfile = open("{}/dadosuniversidades.xml".format(path), "w")
+            myfile.write(xml)
+        
+            print("xml e json salvos nas pastas xmlfiles e jsonfiles, respectivamente, com o nome de dadosuniversidades.")
+        
         
 def main():
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f",help = "acervo no formato csv ou xlsx",required = True)
+    parser.add_argument("-f",help = "acervo no formato csv ou xlsx",required=True)
     parser.add_argument("-t",help = "usado para obter os titulos do arquivo",action = "store_true")
-    parser.add_argument("-i",help = "nome do titulo")
+    parser.add_argument("-col",help = "nome do titulo")
     parser.add_argument("-c",help = "usado para inspecionar o conteudo do titulo",action = "store_true")
-    parser.add_argument("-n",help = "usado para gerar dois novos arquivos csv",action = "store_true")
-    parser.add_argument("-j",help = "usado para gerar o arquivo json",action = "store_true")
-    parser.add_argument("-new",help= "titulo novo")
+    parser.add_argument("-list",help = "usado para gerar dois novos arquivos csv",action = "store_true")
+    parser.add_argument("-json",help = "usado para gerar o arquivo json",action = "store_true")
+    parser.add_argument("-to",help= "titulo novo")
+    parser.add_argument("-xml",help="usado para gerar o arquivo xml",action = "store_true")
 
     args      = parser.parse_args()
     validator = re.search('\.(.*)',args.f).group()
     name      = re.search('.*(?=\.)',args.f).group()
     
     if (args.t):
-        print_titulos(validator, args.f)
+        print_titulos(validator, 'acervos/'+args.f)
     
     if(args.c):
-        if(args.i==None):
+        if(args.col==None):
             print("Erro: Voce esqueceu o nome do titulo")
         else:
-            print_coluna(validator,args.f,args.i)
+            print_coluna(validator,'acervos/'+args.f,args.col)
            
-    if(args.n):
+    if(args.list):
         if validator == '.csv':
-            df = Acervo(pd.read_csv(args.f),args.i, name)
+            df = Acervo(pd.read_csv('acervos/'+args.f),args.col, name)
             df.only_isbn()
         else:
-            df = Acervo(pd.read_excel(args.f),args.i, name)
+            df = Acervo(pd.read_excel('acervos/'+args.f),args.col, name)
             df.only_isbn()
         
-    if(args.j):
+    if(args.json)or(args.xml):
         if validator == '.csv':
             name_df   = re.search('.*(?=\.)',args.f).group() + "_novo.csv" 
             name_list = re.search('.*(?=\.)',args.f).group() + "_isbn.csv"
-            df        = pd.read_csv(name_df)
-            isbn_list = pd.read_csv(name_list)
-            constructor = Constructor(df, isbn_list, args.i, args.new)
+            df        = pd.read_csv('New_csvs/'+name_df)
+            isbn_list = pd.read_csv('ISBN_lists/'+name_list)
+            constructor = Constructor(df, isbn_list, args.col, args.to,args.xml)
             constructor.generate_json()
         else:
             name_df   = re.search('.*(?=\.)',args.f).group() + "_novo.csv" 
             name_list = re.search('.*(?=\.)',args.f).group() + "_isbn.csv"
-            df        = pd.read_csv(name_df)
-            isbn_list = pd.read_csv(name_list)
-            constructor = Constructor(df, isbn_list, args.i, args.new)
+            df        = pd.read_csv('New_csvs/'+name_df)
+            isbn_list = pd.read_csv('ISBN_lists/'+name_list)
+            constructor = Constructor(df, isbn_list, args.col, args.to,args.xml)
             constructor.generate_json()
-                   
+    
 if __name__ == "__main__":
     main()
-    
+
+path = './Jsonfiles'
+
+if os.path.isfile('{}/dadosuniversidades.json'.format(path)):
+    with open(path+'/dadosuniversidades.json') as json_file:
+        my_dict = json.load(json_file)
+
 
